c607e93c597aec41aa7c4b8c0d2f9f5de496a7ff
Karina <>
2023-10-01 08:55:42 +0300

    Начинаем работу с домашним заданием

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.ExceptionServices;
+using Program;
+
+namespace Program {
+    class Program {
+      static void Main() {
+
+
+
+
+
+
+        
+      } 
+    }
+
+
+
+}
+
+    
